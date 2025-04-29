a92c6101a625c3cee0f8c21f1683dd2d9a535e67
anteno12 <okeeffeadam207@gmail.com>
2025-04-29 15:40:07 +1000

    added articles and return to top button on all pages

--- scripts/glossary.js
+++ scripts/glossary.js
@@ -38,9 +38,14 @@ function load(rows) {
 }
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
